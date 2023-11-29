61560f68cedfbab5b202368d6d1278d10b9e7ef3
Beau Lazear <beaulazear@gmail.com>
2023-11-29 17:39:01 -0500

    added birthdate validation

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -9,5 +9,14 @@ class Pet < ApplicationRecord
   validates :address, presence: true
   validates :supplies_location, presence: true
   validates :behavorial_notes, presence: true
+  validates :birthdate, presence: true
+
+  before_validation :ensure_birthdate_is_valid
+
+  def ensure_birthdate_is_valid
+    if birthdate > Date.current
+      errors.add(:birthdate, "Must be in the past")
+    end
+  end
 
 end
