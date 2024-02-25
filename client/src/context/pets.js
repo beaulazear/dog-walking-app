4cc8a7a1a44fbc6094c53c8e26f1f3a651898f93
Beau <beaulazear@gmail.com>
2024-02-24 19:26:06 -0500

    changed sorting function to work on an array of objects

--- client/src/context/pets.js
+++ client/src/context/pets.js
@@ -6,11 +6,29 @@ function PetsProvider({ children }) {
 
     const [pets, setPets] = useState(null)
 
+    console.log(pets)
+
+    function sortObjectsByName(objects) {
+        return objects.sort((a, b) => {
+            const nameA = a.name.toLowerCase();
+            const nameB = b.name.toLowerCase();
+
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
+
     useEffect(() => {
         fetch("/pets").then((response) => {
             if (response.ok) {
                 response.json().then((pets) => {
-                    const sortedPets = pets.sort()
+                    const sortedPets = sortObjectsByName(pets)
                     setPets(sortedPets)
                 });
             }
