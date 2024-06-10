09e97970ff3bbc88d9339bdf321de8bcfbc83939
Beau <beaulazear@gmail.com>
2024-06-10 10:10:05 -0400

    hopefully quick fix

--- client/src/context/todaysAppointments.js
+++ client/src/context/todaysAppointments.js
@@ -9,25 +9,29 @@ const isTodayOrRecurring = (appointment) => {
     const datePart = appointment.appointment_date.slice(5, 7) + '/' + appointment.appointment_date.slice(8, 10) + '/' + appointment.appointment_date.slice(0, 4);
     const currentDate = new Date().toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
 
-    if (!appointment.recurring & !appointment.canceled) {
-        return datePart === currentDate && !hasCancellationToday(appointment.cancellations);
-    } else if (appointment.recurring & !appointment.canceled) {
+    const hasCancellations = appointment.hasOwnProperty('cancellations');
+    const isNotCanceled = !appointment.canceled;
+    const noCancellationToday = hasCancellations ? !hasCancellationToday(appointment.cancellations) : true;
+
+    if (!appointment.recurring && isNotCanceled) {
+        return datePart === currentDate && noCancellationToday;
+    } else if (appointment.recurring && isNotCanceled) {
         const dayOfWeek = today.day();
         switch (dayOfWeek) {
             case 0:
-                return appointment.sunday === true && !hasCancellationToday(appointment.cancellations);
+                return appointment.sunday === true && noCancellationToday;
             case 1:
-                return appointment.monday === true && !hasCancellationToday(appointment.cancellations);
+                return appointment.monday === true && noCancellationToday;
             case 2:
-                return appointment.tuesday === true && !hasCancellationToday(appointment.cancellations);
+                return appointment.tuesday === true && noCancellationToday;
             case 3:
-                return appointment.wednesday === true && !hasCancellationToday(appointment.cancellations);
+                return appointment.wednesday === true && noCancellationToday;
             case 4:
-                return appointment.thursday === true && !hasCancellationToday(appointment.cancellations);
+                return appointment.thursday === true && noCancellationToday;
             case 5:
-                return appointment.friday === true && !hasCancellationToday(appointment.cancellations);
+                return appointment.friday === true && noCancellationToday;
             case 6:
-                return appointment.saturday === true && !hasCancellationToday(appointment.cancellations);
+                return appointment.saturday === true && noCancellationToday;
             default:
                 return false;
         }
@@ -45,11 +49,11 @@ function TodaysAppointmentsProvider({ children }) {
 
     useEffect(() => {
         fetch("/appointments")
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Failed to fetch appointments');
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to fetch appointments');
                 }
             })
             .then((appointments) => {
@@ -62,11 +66,10 @@ function TodaysAppointmentsProvider({ children }) {
                 setTodaysAppointments(sortedAppointments);
             })
             .catch(error => console.error(error));
-        }, []);
-        
-        
-        return (
-            <TodaysAppointmentsContext.Provider value={{ todaysAppointments, setTodaysAppointments }}>
+    }, []);
+
+    return (
+        <TodaysAppointmentsContext.Provider value={{ todaysAppointments, setTodaysAppointments }}>
             {children}
         </TodaysAppointmentsContext.Provider>
     );
