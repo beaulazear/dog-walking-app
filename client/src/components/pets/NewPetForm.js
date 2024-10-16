60bbaefad8dcd8f35a24c385bf0bb2cc52a21c5d
Beau <beaulazear@gmail.com>
2024-10-16 00:07:39 -0400

    changed padding on new oet form

--- client/src/components/pets/NewPetForm.js
+++ client/src/components/pets/NewPetForm.js
@@ -10,6 +10,7 @@ const StyledContainer = styled(Container)`
     background-color: #f8f9fa;
     border-radius: 10px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 20px;
 `;
 
 const StyledTitle = styled.h1`
