9bedb2e4f714b417db8bcf29691550d710911018
Beau Lazear <beaulazear@gmail.com>
2025-11-06 18:25:30 -0500

    addcoors orgin for trainer app on gh pages

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'
+    origins 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175',
+            'https://beaulazear.github.io'
 
     resource '*',
              headers: :any,
