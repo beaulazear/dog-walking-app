5dff6d507290877926e265083b075dbc57530808
Beau Lazear <beaulazear@gmail.com>
2025-11-07 23:10:41 -0500

    seedies

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,3 +11,6 @@ gem install nokogiri --platform=ruby
 bundle install
 
 bundle exec rake db:migrate
+
+# Seed default books only if they don't exist (prevents duplicates on redeployment)
+bundle exec rails runner "Book.defaults.any? || load(Rails.root.join('db', 'seeds', 'books_seed.rb'))"
