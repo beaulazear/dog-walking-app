ce35be2e953c50d16d12a76ac1f81eb8f0629735
Beau Lazear <beaulazear@gmail.com>
2026-01-02 14:14:37 -0500

    fix serializer bug causing render fail

--- app/serializers/pet_sit_completion_serializer.rb
+++
@@ -1,7 +0,0 @@
-class PetSitCompletionSerializer < ActiveModel::Serializer
-  attributes :id, :pet_sit_id, :completion_date, :completed_at,
-             :completed_by_user_id, :created_at
-
-  belongs_to :pet_sit
-  belongs_to :completed_by_user, class_name: 'User'
-end
