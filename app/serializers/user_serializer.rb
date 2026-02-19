6b7cbf0a16c2fb610efd01e1aa685ad4a6173b58
Beau Lazear <beaulazear@gmail.com>
2026-02-19 09:09:56 -0500

    Add admin field to UserSerializer
    - Include admin status in serialized user responses
    - Fixes admin authentication check in frontend

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -22,6 +22,7 @@ class UserSerializer
       sibling_rate: user.sibling_rate,
       pet_sitting_rate: user.pet_sitting_rate,
       profile_pic_url: user.profile_pic_url,
+      admin: user.admin,
       created_at: user.created_at,
       pets: PetSerializer.serialize_collection(pets),
       appointments: AppointmentSerializer.serialize_collection(appointments),
@@ -47,6 +48,7 @@ class UserSerializer
       sibling_rate: user.sibling_rate,
       pet_sitting_rate: user.pet_sitting_rate,
       profile_pic_url: user.profile_pic_url,
+      admin: user.admin,
       created_at: user.created_at
     }
   end
